05d4591486f22b6f6ef7052b7f32b753edd0020b
Micah N Gorrell <m@minego.net>
2014-07-19 13:52:21 -0600

    Allow swipe with mouse events as well as touch events

--- swipe.js
+++ swipe.js
@@ -1,72 +1,114 @@
-function swipeSetup()
+function swipe()
 {
 	var startx, starty;
+	var moved;
 
-	window.addEventListener("touchstart", function(e) {
+	window.addEventListener("touchstart",	this.touchStart.bind(this));
+	window.addEventListener("touchmove",	this.touchMove.bind(this));
+	window.addEventListener("touchend",		this.touchEnd.bind(this));
+
+	window.addEventListener("mousedown",	this.touchStart.bind(this));
+	window.addEventListener("mousemove",	this.touchMove.bind(this));
+	window.addEventListener("mouseup",		this.touchEnd.bind(this));
+};
+
+swipe.prototype.touchStart = function(e)
+{
+	this.moved = false;
+
+	if (e.touches) {
 		if (e.touches.length > 1) {
 			/* Ignore events with more than one finger */
 			return;
 		}
 
-		startx = e.touches[0].clientX;
-		starty = e.touches[0].clientY;
+		e.clientX = e.touches[0].clientX;
+		e.clientY = e.touches[0].clientY;
+	}
 
+	this.startx = e.clientX;
+	this.starty = e.clientY;
+};
+
+swipe.prototype.touchMove = function(e)
+{
+	/*
+		Prevent all move events except the first to allow click events to
+		work as normal.
+	*/
+	if (!this.moved) {
+		this.moved = true;
+	} else {
 		e.preventDefault();
-	});
+	}
+};
 
-	window.addEventListener("touchmove", function(e) {
-		e.preventDefault();
-	});
-
-	window.addEventListener("touchend", function(e) {
+swipe.prototype.touchEnd = function(e)
+{
+	if (e.touches) {
 		if (e.touches.length > 1) {
 			/* Ignore if there is still a finger touching */
 			return;
 		}
 
-		var dx = e.changedTouches[0].clientX - startx;
-		var dy = e.changedTouches[0].clientY - starty;
+		e.clientX = e.changedTouches[0].clientX;
+		e.clientY = e.changedTouches[0].clientY;
+	}
 
-		var ax = Math.abs(dx);
-		var ay = Math.abs(dy);
+	var dx = e.clientX - this.startx;
+	var dy = e.clientY - this.starty;
 
-		if (Math.abs(ax - ay) < 5) {
-			/* Couldn't tell which axis the swipe was meant to be on */
-			return;
-		}
+	var ax = Math.abs(dx);
+	var ay = Math.abs(dy);
 
-		if (Math.max(ax, ay) < 10) {
-			/* Gotta move further than that */
-			return;
-		}
+	if (Math.max(ax, ay) < 10) {
+		return;
+	}
 
-		/* Set up a keydown event */
-		var		ne;
+	if (Math.abs(ax - ay) < 5) {
+		/* Couldn't tell which axis the swipe was meant to be on */
+		return;
+	}
 
-		if (document.createEvent) {
-			ne = document.createEvent("HTMLEvents");
-			ne.initEvent("keydown", true, true);
-		} else {
-			ne = document.createEventObject();
-			ne.eventType = "keydown";
-		}
-		ne.eventName = "keydown";
+	/* Set up a keydown event */
+	var ne = this.createEvent("keydown");
 
-		/* Set the appropriate keycode on the fake event */
-		if (ax > ay) {
-			ne.keyCode = dx > 0 ? 39 : 37;
-		} else {
-			ne.keyCode = dy > 0 ? 40 : 38;
-		}
+	/* Set the appropriate keycode on the fake event */
+	if (ax > ay) {
+		ne.keyCode = dx > 0 ? 39 : 37;
+	} else {
+		ne.keyCode = dy > 0 ? 40 : 38;
+	}
 
-		/* Send the event */
-		if (document.createEvent) {
-			window.dispatchEvent(ne);
-		} else {
-			window.fireEvent("on" + ne.eventType, ne);
-		}
-	});
+	/* Send the event */
+	this.fireEvent(ne);
 };
-swipeSetup();
+
+swipe.prototype.createEvent = function(name)
+{
+	var		e;
+
+	if (document.createEvent) {
+		e = document.createEvent("HTMLEvents");
+		e.initEvent(name, true, true);
+	} else {
+		e = document.createEventObject();
+		e.eventType = name;
+	}
+	e.eventName = name;
+
+	return(e);
+};
+
+swipe.prototype.fireEvent = function(e)
+{
+	if (document.createEvent) {
+		window.dispatchEvent(e);
+	} else {
+		window.fireEvent("on" + e.eventType, e);
+	}
+};
+
+new swipe();
 
 
